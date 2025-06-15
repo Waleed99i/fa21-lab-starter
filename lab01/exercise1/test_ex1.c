e2e95bd2971ef29e8142372fac2ff714c87b7783
Waleed99i <waleedakram059@gmail.com>
2025-06-16 01:42:24 +0500

    Completed exercise1 by waleed

--- lab01/exercise1/test_ex1.c
+++ lab01/exercise1/test_ex1.c
@@ -16,6 +16,11 @@ int main(int argc, char **argv) {
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
 
+    // New test case: test with empty string
+    int num_empty = num_occurrences("", 'a');
+    assert(num_empty == 0);
+
+
     printf("Congrats! If you have made it to this line, your Part 1 Test cases are all passing!\n");
 
     /************ Part 2 Test Cases ************/
@@ -36,6 +41,14 @@ int main(int argc, char **argv) {
     assert(dna_seq_2.T_count == 0);
 
     /* TODO: Think of a scenario that is not tested by the current test cases. Create one additional test case to test this scenario. */
+
+    DNA_sequence dna_seq_3;
+    strcpy(dna_seq_3.sequence, "GGGGGGGGGGGGGGGGGGGG");  // 20 G's
+    compute_nucleotide_occurrences(&dna_seq_3);
+    assert(dna_seq_3.A_count == 0);
+    assert(dna_seq_3.C_count == 0);
+    assert(dna_seq_3.G_count == 20);
+    assert(dna_seq_3.T_count == 0);
 
     printf("Congrats! If you have made it to this line, your Part 2 Test cases are all passing!\n");
 
