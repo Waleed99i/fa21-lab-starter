a4d4578376eff05d97f9d9bfbf5b96fea8d56b10
Waleed99i <waleedakram059@gmail.com>
2025-06-17 13:43:22 +0500

    Completed LAB 03 by Waleed

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -21,4 +21,22 @@ main:
     ecall # Exit
 
 factorial:
-    # YOUR CODE HERE
+    # here in my code
+    # a0 contains n
+    # We'll use:
+    # t0 for i (which iz loop variable)
+    # t1 for result
+
+    li t0, 2         # t0 = 2 (means i = 2)
+    li t1, 1         # t1 = 1 (means result = 1)
+
+loop:
+    bgt t0, a0, done # if i > n, exit loop bcz condition of loop was i<=n and when i>n , it should be ended 
+    mul t1, t1, t0   # means result=result*i which is same as result *= i that we needed for now !!
+    addi t0, t0, 1   # means i=i+1 which is i++
+    j loop
+
+done:
+    mv a0, t1        # return result in a0
+    jr ra            # return to caller
+
